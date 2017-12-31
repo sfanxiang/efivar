5e61362d8eacfe24e63e2882db7cab6a6a177c92
sfanxiang <sfanxiang@gmail.com>
2018-01-01 02:03:05 +0800

    update

--- src/include/efivar/efivar.h
+++ src/include/efivar/efivar.h
@@ -35,6 +35,9 @@
 #define bswap_16(x) __builtin_bswap16(x)
 #define bswap_32(x) __builtin_bswap32(x)
 #define bswap_64(x) __builtin_bswap64(x)
+#define __bswap_16(x) bswap_16(x)
+#define __bswap_32(x) bswap_32(x)
+#define __bswap_64(x) bswap_64(x)
 #endif
 
 typedef struct {
